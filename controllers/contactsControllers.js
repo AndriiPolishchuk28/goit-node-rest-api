87df2b2d3837de2c334ff4de6b4286936a406e42
AndriiPolishchuk28 <ipolishchuk94@gmail.com>
2024-02-23 20:01:35 +0100

    finish

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,66 @@
 import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
+import {
+  createContactSchema,
+  updateContactSchema,
+} from "../schemas/contactsSchemas.js";
 
-export const getAllContacts = (req, res) => {};
+const { listContacts, getContactById, addContact, editContact, removeContact } =
+  contactsService;
 
-export const getOneContact = (req, res) => {};
+export const getAllContacts = async (_, res, next) => {
+  try {
+    const result = await listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const contact = await getContactById(id);
+    if (!contact) {
+      throw HttpError(404);
+    }
+    res.json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await removeContact(id);
+    if (!result) throw HttpError(404);
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+  try {
+    const { error } = createContactSchema.validate(req.body);
+    if (error) throw HttpError(400, error.message);
+    const result = await addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  try {
+    const { error } = updateContactSchema.validate(req.body);
+    if (error) throw HttpError(400, error.message);
+    const { id } = req.params;
+    const result = await editContact(id, req.body);
+    if (!result) throw HttpError(404);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
