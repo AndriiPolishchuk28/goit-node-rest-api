beae7a2b57c0a32f96180d222d8a30ff0793d150
AndriiPolishchuk28 <ipolishchuk94@gmail.com>
2024-02-24 13:18:47 +0100

    validation_body

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -43,8 +43,6 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
   try {
-    const { error } = createContactSchema.validate(req.body);
-    if (error) throw HttpError(400, error.message);
     const result = await addContact(req.body);
     res.status(201).json(result);
   } catch (error) {
@@ -54,8 +52,6 @@ export const createContact = async (req, res, next) => {
 
 export const updateContact = async (req, res, next) => {
   try {
-    const { error } = updateContactSchema.validate(req.body);
-    if (error) throw HttpError(400, error.message);
     const { id } = req.params;
     const result = await editContact(id, req.body);
     if (!result) throw HttpError(404);
